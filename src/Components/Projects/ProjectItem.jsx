a6ba977f3bdc2bfc58f0843911db7e32e0b28bd8
Marek Carlisle <ruby@Mareks-MacBook-Pro.local>
2021-08-10 19:03:28 -0500

    added images for project items

--- src/Components/Projects/ProjectItem.jsx
+++ src/Components/Projects/ProjectItem.jsx
@@ -17,12 +17,23 @@ class ProjectItem extends Component {
 
                 <div id='projectItem'>
 
-                    <a id="itemLink" target="_blank" href={this.props.data.path}>{this.props.data.title}</a>
-                    
+                    <a id="itemLink" target="_blank" href={this.props.data.github}>{this.props.data.title}</a>
+
                     <p id='itemDescription'>{this.props.data.description}</p>
 
                     <p id='itemStatus'>Status: {this.props.data.status}</p>
 
+                    {this.props.data.image !== null &&
+                        <img src={this.props.data.image} alt={this.props.data.image} height="200px"></img>
+                    }
+
+                    {this.props.data.image2 !== null &&
+                        <img src={this.props.data.image2} alt={this.props.data.image2} height="200px"></img>
+                    }
+
+                    {this.props.data.image3 !== null &&
+                        <img src={this.props.data.image3} alt={this.props.data.image3} height="200px"></img>
+                    }
 
                 </div>
 
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -12,11 +12,25 @@ class Projects extends Component {
     state = {
 
         projectItemData: [
-            { title: "My Web Home", path: "https://github.com/Marek-MC/My-Web-Home", heroku: "https://marek-carlisles-website.herokuapp.com/#/home", image: "Null", status: "Base Complete", description: "The website that you are viewing! This website will be used to show professional work, and basic information." },
-            { title: "Moon Moon DiscordJS", path: "https://github.com/Marek-MC/Moon-Moon-Discord-JS", heroku: "Null", image: "Null", status: "W.I.P.", description: "My favorite project as now. Utilizes the Discord API to manipulate, create, and send messages to my Discord server." },
-            { title: "Chatspace", path: "https://morning-eyrie-70641.herokuapp.com/#/chat", heroku: "https://morning-eyrie-70641.herokuapp.com/#/chat", image: "Null", status: "Discontinued", description: "One of the two final big EDA projects I've done. This project has helped solidify all my fullstack programming skills in JS." },
-            { title: "Dental Rental", path: "https://nameless-falls-97077.herokuapp.com/#/rental_requests", heroku: "https://nameless-falls-97077.herokuapp.com/#/rental_requests", image: "Null", status: "Discontinued", description: "The second of the two final big EDA projects I've completed. Together with a group, we talked through a problem with a client, and program an application that should suit their needs." },
-            { title: "EDA Code Bootcamp Account", path: "https://github.com/marek-carlisle", image: "Null", status: "Complete!", description: "During my time with Emerging Digital Academy (EDA), I learned how to create fullstack web applications. This is the account that I put all the projects in!" },
+            { title: "My Web Home", github: "https://github.com/Marek-MC/My-Web-Home", heroku: "https://marek-carlisles-website.herokuapp.com/#/home", status: "Base Complete", 
+            image: null, image2: null, image3: null,
+            description: "The website that you are viewing! This website will be used to show professional work, and basic information." },
+
+            { title: "Moon Moon Discord JS", github: "https://github.com/Marek-MC/Moon-Moon-Discord-JS", heroku: null, status: "W.I.P.", 
+            image: "/images/moon-moon-js/moon.png", image2: "/images/moon-moon-js/moon2.png", image3: null,
+            description: "Utilizes the Discord API to manipulate, create, and send messages to my Discord server." },
+
+            { title: "Chatspace", github: "https://morning-eyrie-70641.herokuapp.com/#/chat", heroku: "https://morning-eyrie-70641.herokuapp.com/#/chat", status: "Discontinued", 
+            image: "images/chatspace/chatspace.png", image2: "images/chatspace/chatspace2.png", image3: null,
+            description: "One of the two final big EDA projects I've done. This project has helped solidify all my fullstack programming skills in JS." },
+
+            { title: "Dental Rental", github: "https://nameless-falls-97077.herokuapp.com/#/rental_requests", heroku: "https://nameless-falls-97077.herokuapp.com/#/rental_requests", status: "Discontinued", 
+            image: "/images/dental-rental/dr.png", image2: "/images/dental-rental/dr2.png", image3: "/images/dental-rental/dr3.png",
+            description: "The second of the two final big EDA projects I've completed. Together with a group, we talked through a problem with a client, and programmed an application that should suit their needs." },
+
+            { title: "EDA Code Bootcamp Account", github: "https://github.com/marek-carlisle", heroku: null, status: "Complete!", 
+            image: null, image2: null, image3: null,
+            description: "During my time with Emerging Digital Academy (EDA), I learned how to create fullstack web applications. This is the account that I put all the projects in!" },
         ],
     }
 
@@ -28,7 +42,7 @@ class Projects extends Component {
             <div>
                 <h1>Projects</h1>
 
-                <p>All project links point to their GitHub repositories.</p>
+                <p>All project links point to their GitHub repositories, or Heroku deployments.</p>
 
 
 
